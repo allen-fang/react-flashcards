58c77dcc59451825409cc1515e437bcfeb18d3ca
Allen Fang <allenfang08@gmail.com>
2017-07-05 11:14:13 -0700

    initial commit for new card form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import HomePage from './components/HomePage/HomePage';
 import TopicPage from './components/TopicPage/TopicPage_container';
+import NewCard from './components/NewCard/NewCard';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <BrowserRouter>
         <div>
           <Switch>
+            <Route path ="/new" component={NewCard} />
             <Route path="/:topic" component={TopicPage} />
             <Route path="/" component={HomePage} />
           </Switch>
