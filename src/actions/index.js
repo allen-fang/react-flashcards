c466a347c5b49f9a93817e7a08fc6811d3aaae21
Allen Fang <allenfang08@gmail.com>
2017-07-06 14:01:37 -0700

    changed root url to deployed mongo db

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,11 +5,11 @@ export const FETCH_TOPIC = 'fetch_topic';
 export const CREATE_CARD = 'create_card';
 export const DELETE_CARD = 'delete_card';
 
-const ROOT_URL = "http://localhost:8080/topics";
+const ROOT_URL = "https://reactflashcards.herokuapp.com";
 
 export function fetchTopic(topic){
 
-  const request = axios.get(`${ROOT_URL}/${topic}`);
+  const request = axios.get(`${ROOT_URL}/topics/${topic}`);
 
   return {
     type: FETCH_TOPIC,
@@ -18,7 +18,7 @@ export function fetchTopic(topic){
 }
 
 export function createCard(values, callback){
-  const request = axios.post(`${ROOT_URL}`, values)
+  const request = axios.post(`${ROOT_URL}/topics`, values)
     .then(() => callback());
 
   return {
@@ -28,7 +28,7 @@ export function createCard(values, callback){
 }
 
 export function deleteCard(id, callback) {
-  const request = axios.delete(`${ROOT_URL}/${id}`)
+  const request = axios.delete(`${ROOT_URL}/topics/${id}`)
     .then(() => callback());
 
   return {
