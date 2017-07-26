3c460b92719396e24d39580533997f493b5f5fb8
Allen Fang <allenfang08@gmail.com>
2017-07-26 16:44:04 -0700

    added build files

--- src/reducers/reducer_topics.js
+++ src/reducers/reducer_topics.js
@@ -14,25 +14,3 @@ export default function (state = {}, action){
   }
 
 
-
-
-// const examplestate = [
-// {id: 1, name: 'javascript', question: 'what is javascript', answer: 'a language'},
-// {id: 2, name: 'javascript', question: 'who is the strongest', answer: 'me'},
-// {id: 3, name: 'javascript', question: 'who is the tallest', answer: 'you'},
-// {id: 4, name:  'python', question: 'what is python', answer: 'a snake'},
-// {id: 5, name:  'python', question: 'what is a snakes favorite food', answer: 'vegetables'},
-// {id: 6, name:  'python', question: 'what do snakes drink', answer: 'water'},
-// {id: 7, name: 'ios', question: 'what is ios', answer:'?'},
-// {id: 8, name: 'ios', question: 'who uses ios', answer:'apple'},
-// {id: 9, name: 'ios', question: 'how', answer:'eee'},
-// {id: 10,name: 'behavioral', question: 'tell me about yourself', answer: 'no'},
-// {id: 11, name: 'behavioral', question: 'what do you like to eat', answer: 'pizza'},
-// {id: 12, name: 'behavioral', question: 'what do you like to drink', answer: 'water'},
-// {id: 13, name: 'web Development', question: 'what is your name', answer: 'who'},
-// {id: 14, name: 'web Development', question: 'what is your last name', answer: 'huh'},
-// {id: 15, name: 'web Development', question: 'who are you', answer: 'person'},
-// {id: 16, name: 'databases', question: 'what is a db', answer: 'database'},
-// {id: 17, name: 'databases', question: 'what is sql', answer: 'a db'},
-// {id: 18, name: 'databases', question: 'what is mongo', answer: 'a db'}
-// ];
